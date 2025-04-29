2f536d73f96bdc5ee3387cb1567cf94f5160925c
Felipe Eugenio <justworkttk@gmail.com>
2025-04-29 16:45:00 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 80
+      },
+      "color": {
+        "value": "#000000"
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#000000",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2
+      },
+      "size": {
+        "value": 2
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "grab"  // Você pode trocar por "repulse", "bubble" etc.
+        }
+      }
+    },
+    "retina_detect": true
+  });
+  
